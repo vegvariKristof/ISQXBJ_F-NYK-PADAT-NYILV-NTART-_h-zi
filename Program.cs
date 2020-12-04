08bfd68b0d50e21b3b7d9c6401ea9add2111ffc6
vegvariKristof <vegvari.kristof99@gmail.com>
2020-12-04 15:27:47 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ISQXBJ_FÉNYKÉPADAT_NYILVÁNTARTÓ_házi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string s;
+        }
+    }
+}
